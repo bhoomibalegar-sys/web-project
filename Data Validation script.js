f7ca6ee00a93d26baad1a360da97eae1936f928a
bhoomibalegar-sys <bhoomibalegar@gmail.com>
2026-04-04 15:49:20 +0530

    Add form validation function

---
+++ Data Validation script.js
@@ -0,0 +1,11 @@
+function validateForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+
+  if (name === "" || email === "") {
+    document.getElementById("error").innerText = "All fields are required!";
+    return false;
+  }
+
+  return true;
+}
